285e4fab1c291c0dac366068c95e01c84559924d
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-12-26 22:22:53 +0530

    inital commit

---
+++ src/test/java/com/thilina271/Ecomm_application/EcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilina271.Ecomm_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
